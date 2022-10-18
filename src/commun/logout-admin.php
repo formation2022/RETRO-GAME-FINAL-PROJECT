247841114d47ee72f0f42c246113b13e9ac85569
formation2022 <103824522+formation2022@users.noreply.github.com>
2022-10-18 15:38:51 +0200

    Delete src/commun directory
    en double!!

--- src/commun/logout-admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start(); // INITIALISE LA SESSION
-session_unset(); // DESACTIVE LA SESSION
-session_destroy(); // DETRUIT LA SESSION
-setcookie('auth', '', time() - 1, '/', null, false, true); // DETRUIT LE COOKIE
-
-header('location: ../src/dashboard.php');
-exit();
